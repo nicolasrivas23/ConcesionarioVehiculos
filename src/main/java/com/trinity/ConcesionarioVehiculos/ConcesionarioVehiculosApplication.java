5ac6b081d0c4b9a87820976846f6b2d6a5b17ca0
nicolasrivas23 <nicolasrivasmeneses23@gmail.com>
2023-03-21 09:27:01 -0500

    Inicio del desarrollo

---
+++ src/main/java/com/trinity/ConcesionarioVehiculos/ConcesionarioVehiculosApplication.java
@@ -0,0 +1,13 @@
+package com.trinity.ConcesionarioVehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcesionarioVehiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcesionarioVehiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinity/ConcesionarioVehiculos/ConcesionarioVehiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinity.ConcesionarioVehiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcesionarioVehiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
